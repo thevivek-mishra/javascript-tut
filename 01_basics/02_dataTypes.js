02ea82a4fe83ac10052a0e93bab0a9eed47e2cb5
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-01-06 22:06:27 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3);// code readability should be high 
+    
+console.log("Hitesh")
+
+let name ="hitesh"
+let age =18;
+let isLoggedIn = false;
+
+//1. number => 2 to power 53
+//2. bigint
+//3. string =>""
+//4. boolean =>true/false
+//5. null=> standalone value
+//6. undefined =>
+//7. symbol 
+//8. Object
+
+console.log(typeof null) // null
+console.log(typeof undefined) // undefined
