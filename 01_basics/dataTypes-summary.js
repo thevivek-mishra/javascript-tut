80b6b19f829c6eb8a00bf6eb3d12b0bc5d4d51e9
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-01-07 22:09:21 +0530

    datatypes-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive 
+// 7 types : string, Number, Boolean, null, undefined, Symbol,BigInt
+
+// reference (non primitive)
+// Array, Objects, Functions
+
+// Symbol
+
+const id = Symbol('123')
+
+const anotherId =Symbol('123')
+
+console.log(id === anotherId)
+
+// bitInt 
+
+const bigNumber = 33345343n;
+
+// array 
+
+const heros =["shaktiman","naagra","doga"]
+
+// object
+
+let myObj ={
+    name:"vivek",
+    age: 23,
+
+}
+// function
+
+const myFunction =function(){
+    console.log("Hellow world");
+}
+
+console.log(typeof myFunction) // function
+
+
+
+
+
