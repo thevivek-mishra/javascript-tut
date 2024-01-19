b58369cda7a5540b51b161d99b10a9a93c75beb5
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-01-19 22:04:04 +0530

    object part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,37 @@
+// singleton
+//  Object.create()
+
+// Object literals
+
+const mySym =Symbol("key1")
+
+const jsUser={
+    name:"viek", //by default "name":"vivek"
+    "full name":"vivek kumar mishra",
+    age:18,
+    location:"Noida",
+    email:"vivek@gmail.com",
+    [mySym]:"mykey1",
+    isLoggedIn:false,
+    lastLoginDays: ["Monday","saturday"]
+
+}
+
+console.log(jsUser.name)
+console.log(jsUser["full name"])
+console.log(jsUser[mySym])
+
+jsUser.email="mishra@gmail.com"
+
+// Object.freeze() // this is used to freeze the object after that we can not change anything in object
+
+jsUser.greeting =function (){
+    console.log("hellow js user")
+}
+
+
+jsUser.greetingTwo(){
+    console.log(`hellow js user, ${this.name}`)
+}
+
+
