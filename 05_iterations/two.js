7cdb536c294baca9a024cd028879c4f18865aaec
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-01-20 23:14:22 +0530

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let index =0;
+while (index <= 10) {
+    console.log(`value of index is ${index}`)
+    index =index+2
+}
+
+const myArray =["flash","batman","superman"]
+let arr =0
+while(acc <myArray.length){
+    console.log(`value is ${myArray[arr]} `);
+    arr =arr +1
+}
+let score =1
+do{
+console.log(`score is ${score}`)
+score++
+}
+while(score <=10);
