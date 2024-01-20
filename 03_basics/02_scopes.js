545cdd55f2e58cd28cf60c51228b535eb45e43da
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-01-20 21:06:56 +0530

    hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,14 @@ const arr =[2,3,4]
 for(let i=0;i<arr.length;i++){
     const element =arr[i]
 }
+
+console.log(add(5)) // can be hoisted
+
+function add (num){
+    return num+1
+}
+
+console.log(add(5)) // can not be hoisted
+const add2 =function(num){
+    return num+1
+}
